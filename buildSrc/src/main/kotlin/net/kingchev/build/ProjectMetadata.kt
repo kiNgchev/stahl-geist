f3fd7c7262f60523f5b89383d0cdb8c6b9f90bed
KiNgchev <kingchev.net@gmail.com>
2025-06-15 23:46:37 +0300

    chore: update ProjectMetadata.kt

--- buildSrc/src/main/kotlin/net/kingchev/build/ProjectMetadata.kt
+++ buildSrc/src/main/kotlin/net/kingchev/build/ProjectMetadata.kt
@@ -1,5 +1,19 @@
 package net.kingchev.build
 
+
 object ProjectMetadata {
-    const val VERSION: String = "0.0.1-SNAPSHOT"
+    const val PREFIX = "stahl-"
+    const val PACKAGE: String = "net.kingchev"
+    val VERSION: String = Version.getVersion()
+}
+
+object Version {
+    const val MAJOR: String = "1"
+    const val MINOR: String = "0"
+    const val PATCH: String = "0"
+    const val IS_SNAPSHOT = false
+
+    fun getVersion(): String {
+        return "$MAJOR.$MINOR.$PATCH" + if (IS_SNAPSHOT) "-SNAPSHOT" else ""
+    }
 }
